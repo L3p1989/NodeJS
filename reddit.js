77878e989c88dca8bac90427935c37c837f136a8
Patrick Hogan <phogan110@live.com>
2018-12-24 13:55:29 -0700

    created request promise to grab data from specified url

--- reddit.js
+++ reddit.js
@@ -1,1 +1,20 @@
+const path = require("path");
+const fs = require("fs");
 var rp = require("request-promise");
+
+rp("https://reddit.com/r/popular.json")
+  .then(redditJSON => {
+    let reddit = JSON.parse(redditJSON); //Convert redditJSON to JS
+    let customData = []; //array for data that reddit is being parsed
+
+    reddit.data.children.forEach(post => {
+      customData.push({
+        title: post.data.title,
+        url: post.data.url,
+        author: post.data.author
+      }); //push title, url, and author to customData
+      let postData = JSON.stringify(customData); //convert customData into JSON
+      fs.writeFileSync("popular-articles.json", postData); //write JSON to file
+    });
+  })
+  .catch(e => console.log(e));
