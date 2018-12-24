ff7ca85dee2a1e808e884ad8a2961797c6b2f4c0
Patrick Hogan <phogan110@live.com>
2018-12-24 14:38:30 -0700

    added comments

--- popular-downloader.js
+++ popular-downloader.js
@@ -4,11 +4,11 @@ var rp = require("request-promise");
 
 rp("https://reddit.com/r/popular.json")
   .then(redditJSON => {
-    let reddit = JSON.parse(redditJSON);
+    let reddit = JSON.parse(redditJSON); //Convert redditJSON to JS
     reddit.data.children.forEach(post => {
-      let ext = path.extname(post.data.url);
-      let file = post.data.id;
-      let name = `${file}${ext}`;
+      let ext = path.extname(post.data.url); //shorten code to `ext`
+      let file = post.data.id; //shorten code to `file`
+      let name = `${file}${ext}`; //shorten code to `name`
       if (ext === ".jpg" || ".gif" || ".png") {
         rp(post.data.url, { encoding: "base64" })
           .then(media => {
@@ -22,7 +22,7 @@ rp("https://reddit.com/r/popular.json")
             );
           })
           .catch(e => console.log(e));
-      }
+      } //grab all files with .jpg/.gif/.png and write them to 'downloads' folder with id as file name
     });
   })
   .catch(e => console.log(e));
