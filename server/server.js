84ae292da60f4a8f120adb180fe35bc005f1884e
Patrick Hogan <phogan110@live.com>
2018-12-24 11:00:51 -0700

    completed Part 1

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const fs = require("fs");
+
+let chirps = [
+  { name: "Patrick", chirp: "This Node stuff is pretty cool!" },
+  { name: "Stephanie", chirp: "I don't wanna be at work!" },
+  { name: "Luke", chirp: "Must haz coffee!" },
+  { name: "Paws", chirp: "Please pet me!" },
+  { name: "Saoirse", chirp: "What's that!?" }
+]; //list of chirps
+
+let data = JSON.stringify(chirps); //passes chirps ito JSON
+fs.writeFileSync("chirps.json", data); //writes file chirps.json with data
+
+let rawData = fs.readFileSync("chirps.json"); //reads chirps.json
+let chirpsData = JSON.parse(rawData); //parses chirps.json into JS
+console.log(chirpsData); //outputs JS chirpsData into console
